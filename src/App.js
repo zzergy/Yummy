743346587450aca84f816b65c25c44306e26dcd3
Zergy <xxdarkbluedevilxx@gmail.com>
2021-02-04 13:38:56 +0200

    Basic SignUp design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
-import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Profile from './components/Profile';
 import ViewRecipe from './components/ViewRecipe';
 import './App.css';
+import 'fontsource-roboto';
+
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
         <Router>
           <Switch>
             <Route exact path='/' component={Home}/>
-            <Route path='/signin' component={SignIn}/>
+            <Route path='/signin' component={SignUp}/>
             <Route path='/login' component={Login}/>
             <Route path='/profile' component={Profile}/>
             <Route path='/view-recipe' component={ViewRecipe}/>
---
+++ src/components/SignUp.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {Link} from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+  //wrapper (main container)
+  container: {
+    marginTop: theme.spacing(20),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.container}>
+
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="confirmPassword"
+                label="Confirm password"
+                type="password"
+                id="confirmPassword"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+
+          <Grid container justify="center">
+           <Grid item>
+              Already have an account?<Link to='/login'> Login</Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
