890f27ed8edd458e7f1fe98afbb7317fc92051a1
zzergy <xxdarkbluedevilxx@gmail.com>
2021-02-16 15:28:53 +0200

    Code clean  up and bug fixes
    - Fied the navigation bar underline thickness
    - Removed unnecessary logout functionality from the page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,19 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
-import { AuthenticationContext } from '../context/AuthenticationContext';
-import { useHistory } from 'react-router-dom'
 import { NavigationBar } from './NavigationBar/NavigationBar';
 import './Home.css';
 
 export default function Home() {
-    // const { logout } = useContext(AuthenticationContext);
-    // const [error, setError] = useState('');
-    // const history = useHistory();
-
-
-    // async function handleLogout() {
-    //     try {
-    //         await logout();
-    //         history.push('/login');
-    //     } catch {
-    //         setError('Failed to log out');
-    //     }
-    // }
 
     return (
         <div className='wrapper'>
-
-            <div className='nav'>
+            <div className="navigation">
                 <NavigationBar />
             </div>
 
             <div className='main'>
-                Main
-                </div>
+                MAIN
+            </div>
 
             <div className='footer'>
                 footer
