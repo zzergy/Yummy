00f56b223386345e308f7ee691db673e2e9255a2
Zergy <xxdarkbluedevilxx@gmail.com>
2021-02-23 14:55:08 +0200

    Added firebase to gitignore
    To protect the firebase config, after the project is done this file will be removed from gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,13 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-""
+  apiKey: "AIzaSyA7uLZNj11QETp_2FkxlWgAJJo5Dt7JQK0",
+  authDomain: "yummy-dev-2f6d6.firebaseapp.com",
+  databaseURL: "https://yummy-dev-2f6d6-default-rtdb.firebaseio.com",
+  projectId: "yummy-dev-2f6d6",
+  storageBucket: "yummy-dev-2f6d6.appspot.com",
+  messagingSenderId: "637364639138",
+  appId: "1:637364639138:web:0c7fed5f516a869e13cefb"
   };
 
 const app = firebase.initializeApp(firebaseConfig);
