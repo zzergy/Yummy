8e61b7a2c0ce2e9a2434925a07dfc17f1c86519d
Zergy <xxdarkbluedevilxx@gmail.com>
2021-02-23 18:25:10 +0200

    Profile Routing

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import NavigationBar from '../NavigationBar/NavigationBar'
+import { Link } from 'react-router-dom'
 
 export default function Profile() {
-    return (<div>Profile page</div>)
+    return (
+        <div>
+                <NavigationBar />
+                <Link to="new-recipe">Create Recipe</Link>
+        </div>
+    )
 }
