386b4a63aa86c22d35a4089bf4ba34a35a9d8074
zzergy <xxdarkbluedevilxx@gmail.com>
2021-03-06 07:29:15 +0200

    Database connection

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavigationBar from './NavigationBar/NavigationBar';
 import './Home.css';
 import Footer from './Footer'
 import RecipeItem from './RecipeItem/RecipeItem'
 import { Container, Grid } from '@material-ui/core';
+import firebase from 'firebase/app';
+import "firebase/database";
 
 export default function Home() {
+    const [recipes, setRecipes] = useState();
+
+    useEffect(() => {
+        const recipeRef = firebase.database().ref("recipes");
+        recipeRef.on('value', snapshot => {
+            const recipes = snapshot.val();
+            const recipesList = [];
+            for (let id in recipes) {
+                recipesList.push(recipes[id]);
+            }
+            setRecipes(recipesList);
+        });
+    }, [])
 
     return (
         <div className='wrapper'>
@@ -15,39 +30,15 @@ export default function Home() {
 
             <Container className="main">
                 <Grid container spacing={4}>
-                    <Grid item container justify="center" xs={12} md={6} lg={4}>
-                        <Grid item>
-                            <RecipeItem />
+                    {recipes ? recipes.map((recipe, index) => (
+                        <Grid item container justify="center" xs={12} md={6} lg={4}>
+                            <Grid item>
+                                <RecipeItem recipe={recipe} key={index}/>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                    <Grid item container justify="center" xs={12} md={6} lg={4}>
-                        <Grid item>
-                            <RecipeItem />
-                        </Grid>
-                    </Grid>
-                    <Grid item container justify="center" xs={12} md={6} lg={4}>
-                        <Grid item>
-                            <RecipeItem />
-                        </Grid>
-                    </Grid>
-                    <Grid item container justify="center" xs={12} md={6} lg={4}>
-                        <Grid item>
-                            <RecipeItem />
-                        </Grid>
-                    </Grid>
-                    <Grid item container justify="center" xs={12} md={6} lg={4}>
-                        <Grid item>
-                            <RecipeItem />
-                        </Grid>
-                    </Grid>
-                    <Grid item container justify="center" xs={12} md={6} lg={4}>
-                        <Grid item>
-                            <RecipeItem />
-                        </Grid>
-                    </Grid>
+                    )) : ''}
                 </Grid>
             </Container>
-
             <div className='footer'>
                 <Footer />
             </div>
