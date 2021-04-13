3609bbf7413d03e06a61e9f002c8c570febca2ce
Zergy <xxdarkbluedevilxx@gmail.com>
2021-04-13 12:28:12 +0300

    Removed unused imports

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import NavigationBar from './NavigationBar/NavigationBar';
 import './Home.css';
 import Footer from './Footer'
