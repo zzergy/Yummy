70ef972d67c6737e394688a76d1790f82dca90b1
Zergy <xxdarkbluedevilxx@gmail.com>
2021-04-13 20:37:13 +0300

    Updated useAllRecipesFromDB
    Moved the state directly in the useEffect hook

--- src/useAllRecipesFromDB.js
+++ src/useAllRecipesFromDB.js
@@ -1,20 +1,23 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import firebase from 'firebase/app';
 import "firebase/database";
 /**
  * Gets the data from the Firebase Realtime Database 
- * @param {setState k} setRecipes 
  */
-export default function useAllRecipesFromDB(setRecipes) {
+export default function useAllRecipesFromDB() {
+    const [recipes, setRecipes] = useState([]);
+    
     useEffect(() => {
         const recipeRef = firebase.database().ref("recipes");
         recipeRef.on('value', snapshot => {
             const recipes = snapshot.val();
             const recipesList = [];
             for (let id in recipes) {
-                recipesList.push(recipes[id]);
+                recipesList.push({...recipes[id], id});
             }
             setRecipes(recipesList);
         });
     }, [])
+
+    return recipes;
 }
