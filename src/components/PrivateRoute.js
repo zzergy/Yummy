fc238289ef1e8709c55daa10797f914ec8cddc61
zzergy <xxdarkbluedevilxx@gmail.com>
2021-06-03 16:33:48 +0300

    Changed the rendering conditions

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,12 +3,14 @@ import { Redirect, Route } from "react-router";
 import { AuthenticationContext } from "../context/AuthenticationContext";
 
 export default function PrivateRoute({ component: Component, ...otherProps }) {
-    const { currentUser } = useContext(AuthenticationContext);
+    const { currentUser, loadedUserFromStorage } = useContext(AuthenticationContext);
+
+
 
     return (
         <Route {...otherProps} render={props => (
             currentUser ? <Component {...props} /> :
-                <Redirect to="/login" />
+                loadedUserFromStorage && <Redirect to="/login" />
         )} />
     );
 }
